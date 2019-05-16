746359ff245914a921ebc2ed5d4315d41f036a1f
Christopher Brownlee <cdbreach@gmail.com>
2019-05-15 21:04:31 -0400

    Logon functionality has been completed

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,11 +1,14 @@
 $(function(){
     $('#logData').on('click', function(event){
+        let logMail = $('#logEmail').val().trim();
+        let logPass = $('#logPass').val().trim();
         updateData = {
-            email: $('#logEmail').val().trim(),
-            password: $('#logPass').val().trim()
-        }
-        $.ajax('/verify', {
-            type: 'PUT',
+            email: logMail,
+            password: logPass
+        };
+        console.log(updateData)
+        $.ajax('/api/verify', {
+            type: 'POST',
             data: updateData
         }).then(function(data){
             console.log(data)
