5383c3608d905c14ce14838610224ece382ee0b0
slcassidy <samantha.l.cassidy@gmail.com>
2019-06-27 20:54:05 -0400

    add photo upload process

--- controller/surveyController.js
+++ controller/surveyController.js
@@ -12,12 +12,13 @@ router.get('/survey', function (req, res){
 
 // Posting Survey(Profile) info to db
 router.post('/survey', function(req, res){
-    survey.createsurvey(['first_name', 'last_name', 'gender', 'home_street', 'home_city', 'home_state', 'home_zip', 'mobile','gym_name','gym_street', 'gym_city', 'gym_state', 'gym_zip','primary_training_type', 'secondary_training_type', 'level', 'levelS'], [req.body.firstname, req.body.lastname, req.body.gender, req.body.homestreet, req.body.homecity, req.body.homestate, req.body.homezip, req.body.mobile, req.body.gymname, req.body.gymstreet, req.body.gymcity, req.body.gymstate, req.body.gymzip,req.body.primaryexcer, req.body.secondaryexer, req.body.primarylevel, req.body.secondarylevel], function(data){
+    survey.createsurvey(['first_name', 'last_name', 'gender', 'home_street', 'home_city', 'home_state', 'home_zip', 'mobile','gym_name','gym_street', 'gym_city', 'gym_state', 'gym_zip','primary_training_type', 'secondary_training_type', 'level', 'levelS','photo'], [req.body.firstname, req.body.lastname, req.body.gender, req.body.homestreet, req.body.homecity, req.body.homestate, req.body.homezip, req.body.mobile, req.body.gymname, req.body.gymstreet, req.body.gymcity, req.body.gymstate, req.body.gymzip,req.body.primaryexcer, req.body.secondaryexer, req.body.primarylevel, req.body.secondarylevel, req.body.photo], function(data){
             //Sending some data back to validate
-            const hbsObject = {
-                survey: data
-            };
-            res.render('survey', hbsObject);
+            // const hbsObject = {
+            //     survey: data
+            // };
+            // res.render('survey', hbsObject);
+            res.send(data);
     })
 });
 
