3fcb05d983efcdcab02b5211494441cad6118ebc
Christopher Brownlee <cdbreach@gmail.com>
2019-07-09 20:33:54 -0400

    Corrected survey page error

--- controller/surveyController.js
+++ controller/surveyController.js
@@ -20,17 +20,19 @@ router.post('/survey/:id', function(req, res){
     console.log(req.params.id);
     const cred_id1 = req.params.id;
 
-    survey.createsurvey(['first_name', 'last_name', 'gender', 'home_street', 'home_city', 'home_state', 'home_zip', 'mobile','gym_name','gym_street', 'gym_city', 'gym_state', 'gym_zip','primary_training_type', 'secondary_training_type', 'level', 'levelS','photo', 'cred_id'], [req.body.firstname, req.body.lastname, req.body.gender, req.body.homestreet, req.body.homecity, req.body.homestate, req.body.homezip, req.body.mobile, req.body.gymname, req.body.gymstreet, req.body.gymcity, req.body.gymstate, req.body.gymzip,req.body.primaryexcer, req.body.secondaryexer, req.body.primarylevel, req.body.secondarylevel, req.body.photo, cred_id1], function(data){
+    survey.createsurvey(['first_name', 'last_name','home_street', 'home_city', 'home_state', 'home_zip', 'mobile','gym_name','gym_street', 'gym_city', 'gym_state', 'gym_zip','primary_training_type', 'secondary_training_type', 'level', 'levelS','photo', 'cred_id'], [req.body.firstname, req.body.lastname, req.body.homestreet, req.body.homecity, req.body.homestate, req.body.homezip, req.body.mobile, req.body.gymname, req.body.gymstreet, req.body.gymcity, req.body.gymstate, req.body.gymzip,req.body.primaryexcer, req.body.secondaryexer, req.body.primarylevel, req.body.secondarylevel, req.body.photo, cred_id1], function(data){
             //Sending some data back to validate
             // const hbsObject = {
             //     survey: data
             // };
             // res.render('survey', hbsObject);
 
-
+            
             let bingapiKey = process.env.BING_API_KEY
             let bingURL = ""
+            console.log(bingURL)
             bingURL = `http://dev.virtualearth.net/REST/v1/Locations/US/${req.body.homestate}/${req.body.homezip}/${req.body.homecity}/${req.body.homestreet}?key=${bingapiKey}`
+            console.log(bingURL)
             axios.get(bingURL).then(function(response) {
 
               latitude = response.data.resourceSets[0].resources[0].geocodePoints[0].coordinates[0]
